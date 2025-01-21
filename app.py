c43ed653fce3cbe73216d1b83e179b7428172931
Jayesh Patil <jp7470484@gmail.com>
2025-01-21 12:25:47 +0530

    done

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def health_check():
     return jsonify({'status': 'API is running'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
